109824d611779d29794b8a3f56922be91babb5f1
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-22 11:27:04 +0300

    initial commit

--- random-module/src/com/example/random/service/business/FastRandomIntegerService.java
+++ random-module/src/com/example/random/service/business/FastRandomIntegerService.java
@@ -2,12 +2,16 @@ package com.example.random.service.business;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import com.example.random.service.QualityLevel;
 import com.example.random.service.RandomIntegerService;
+import com.example.random.service.ServiceQuality;
 
+@ServiceQuality(value=QualityLevel.FAST)
 public class FastRandomIntegerService implements RandomIntegerService {
 
 	@Override
 	public int generate(int min, int max) {
+		System.out.println("FastRandomIntegerService::generate");
 		return ThreadLocalRandom.current().nextInt(min, max);
 	}
 
--- random-module/src/com/example/random/service/business/SecureRandomDoubleService.java
+++ random-module/src/com/example/random/service/business/SecureRandomDoubleService.java
@@ -3,15 +3,19 @@ package com.example.random.service.business;
 import java.security.SecureRandom;
 import java.util.Random;
 
+import com.example.random.service.QualityLevel;
 import com.example.random.service.RandomDoubleService;
+import com.example.random.service.ServiceQuality;
 
+@ServiceQuality(value = QualityLevel.SECURE)
 public class SecureRandomDoubleService implements RandomDoubleService {
 
 	private Random secureRandom = new SecureRandom();
 
 	@Override
 	public double generate(double min, double max) {
-		return secureRandom.nextDouble(min, max) ;
+		System.out.println("SecureRandomDoubleService::generate");
+		return secureRandom.nextDouble(min, max);
 	}
 
 }
