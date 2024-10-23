020df3b67357f00c8fc4223136d8213423804f02
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-23 12:23:01 +0300

    Update Exercise7.java

--- study-stream-api/src/com/example/exercises/Exercise7.java
+++ study-stream-api/src/com/example/exercises/Exercise7.java
@@ -1,5 +1,10 @@
 package com.example.exercises;
 
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.example.domain.Genre;
+import com.example.domain.Movie;
 import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
 
@@ -13,7 +18,15 @@ public class Exercise7 {
 
 	public static void main(String[] args) {
 		// Find the list of movies having the genres "Drama" and "Comedy" only
-
+		Genre drama = movieService.findAllGenres().stream().filter(genre -> genre.getName().equals("Drama")).findFirst().get();
+		Genre comedy = movieService.findAllGenres().stream().filter(genre -> genre.getName().equals("Comedy")).findFirst().get();
+        Predicate<Movie> isDrama = movie -> movie.getGenres().contains(drama);
+        Predicate<Movie> isComedy = movie -> movie.getGenres().contains(comedy);
+		Predicate<Movie> has2Genres = movie -> movie.getGenres().size() == 2;
+		movieService.findAllMovies()
+                    .stream()
+                    .filter(isDrama.and(isComedy).and(has2Genres))
+                    .forEach(movie -> System.out.println("%s: %s".formatted(movie.getTitle(),movie.getGenres().stream().map(Genre::getName).collect(Collectors.joining(",", "[", "]")))));
 	}
 
 }
