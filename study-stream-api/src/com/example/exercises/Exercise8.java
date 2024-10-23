1ab7fb25f1a54a3026f51bfd133cde542fd2c67f
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-23 12:33:31 +0300

    Update Exercise8.java

--- study-stream-api/src/com/example/exercises/Exercise8.java
+++ study-stream-api/src/com/example/exercises/Exercise8.java
@@ -1,6 +1,9 @@
 package com.example.exercises;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import com.example.domain.Movie;
 import com.example.service.InMemoryMovieService;
@@ -17,7 +20,14 @@ public class Exercise8 {
 	public static void main(String[] args) {
 		// Group the movies by the year and list them
 		Collection<Movie> movies = movieService.findAllMovies();
-
+        var moviesByYear =
+        movies.stream()
+              .collect(Collectors.groupingBy(Movie::getYear,Collectors.toList()));
+        moviesByYear
+           .entrySet()
+           .stream()
+           .sorted(Comparator.comparingInt(Entry::getKey))
+           .forEach(entry -> System.out.println("%d: (%d) %s".formatted(entry.getKey(),entry.getValue().size(),entry.getValue().stream().map(Movie::getTitle).sorted().collect(Collectors.joining(",", "[", "]")))));
 	}
 
 }
