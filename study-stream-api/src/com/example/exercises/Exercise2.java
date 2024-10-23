bf8ef45c6bede7efd5a639e7b3d1ef01cc57377c
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-23 12:00:25 +0300

    Update Exercise2.java

--- study-stream-api/src/com/example/exercises/Exercise2.java
+++ study-stream-api/src/com/example/exercises/Exercise2.java
@@ -1,7 +1,12 @@
 package com.example.exercises;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.City;
 
 /**
  * 
@@ -13,6 +18,16 @@ public class Exercise2 {
 
 	public static void main(String[] args) {
 		// Find the most populated city of each continent
+		var mostPopulatedCityOfEachContinent =
+		countryDao.findAllCountries()
+		          .stream()            // Stream<Country>
+		          .map( country -> country.getCities().stream().map( city -> new ContinentCityPair(country.getContinent(), city)).toList()) // Stream<ContinentCitiesPair>
+		          .flatMap(List::stream)
+		          .collect(Collectors.groupingBy(ContinentCityPair::continent,Collectors.maxBy( (pair1,pair2) -> pair1.city().getPopulation()-pair2.city().getPopulation())));
+		mostPopulatedCityOfEachContinent.forEach((continent,pair)->System.out.println("%16s: %s,%d".formatted(continent,pair.get().city().getName(),pair.get().city().getPopulation())));
+		          
 	}
 
 }
+
+record ContinentCityPair(String continent,City city) {}
